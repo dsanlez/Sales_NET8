6f9c624f8e22e7f58ef1fa9237a680717ad9468a
Diogo Sanlez <diogosdl25@gmail.com>
2024-07-23 23:18:31 +0100

    Countries controller

--- Sales_NET8/Sales_NET8.Web/Controllers/HomeController.cs
+++ Sales_NET8/Sales_NET8.Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Sales_NET8.Web.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
