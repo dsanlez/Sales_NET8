541a06cb6e04af8a626e4c864be3ab825145302d
Diogo Sanlez <diogosdl25@gmail.com>
2024-07-23 22:25:54 +0100

    InitialDb

---
+++ Sales_NET8/Sales_NET8.Web/Data/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales_NET8.Web.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no mínimo {1} caractéres. ")]
+        public string? Name { get; set; }
+    }
+}
